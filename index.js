2d8959a15ad564fee3f658b87852bb00c2b6d0bc
Chandan <ethicalrobo06@gmail.com>
2024-11-07 00:37:16 +0530

    req and data handling

--- index.js
+++ index.js
@@ -5,19 +5,13 @@ const morgan = require('morgan')
 app.use(morgan('dev'))
 app.set('view engine', 'ejs')
 
-app.get(
-  '/',
-  (req, res, next) => {
-    const a = 5
-    const b = 10
-    console.log(a + b)
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-    next()
-  },
-  (req, res) => {
-    res.render('index')
-  }
-)
+// Routes
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
 app.get('/about', (req, res) => {
   res.send('<h1>About</h1>')
@@ -27,4 +21,9 @@ app.get('/profile', (req, res) => {
   res.send('<h1>Profile</h1>')
 })
 
+app.post('/get-form-data', (req, res) => {
+  //   console.log(req.query) --> ye get req m aata
+  console.log(req.body)
+  res.send('data received')
+})
 app.listen(3000)
