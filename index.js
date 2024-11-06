53a6b703d47f2b2b987c4ed42130c8ed783a04c4
Chandan <ethicalrobo06@gmail.com>
2024-11-07 00:26:43 +0530

    basic middleware

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
+app.use(morgan('dev'))
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
-  res.render('index')
-})
+app.get(
+  '/',
+  (req, res, next) => {
+    const a = 5
+    const b = 10
+    console.log(a + b)
+
+    next()
+  },
+  (req, res) => {
+    res.render('index')
+  }
+)
 
 app.get('/about', (req, res) => {
   res.send('<h1>About</h1>')
