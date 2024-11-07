5343115b71f25699070da84a832e21820b002298
Chandan <ethicalrobo06@gmail.com>
2024-11-07 12:57:54 +0530

    CRUD Operations

--- index.js
+++ index.js
@@ -28,4 +28,59 @@ app.post('/get-form-data', (req, res) => {
   console.log(req.body)
   res.send('data received')
 })
+
+app.get('/register', (req, res) => {
+  res.render('register')
+})
+
+// CRUD Operations
+app.post('/register', async (req, res) => {
+  const { username, email, password } = req.body
+
+  const newUser = await userModel.create({
+    username: username,
+    email: email,
+    password: password,
+  })
+
+  res.send('user register')
+})
+
+app.get('/get-users', async (req, res) => {
+  // -- find always returns []
+  // userModel.find().then((users) => {
+  //   res.send(users)
+  // })
+  // -- findOne returns only one condition which is created first
+  await userModel
+    .findOne({
+      username: 'aashta',
+    })
+    .then((user) => {
+      res.send(user)
+    })
+})
+
+app.get('/update-user', (req, res) => {
+  userModel
+    .updateOne({ username: 'aashta' }, { username: 'pasta' })
+    .then((result) => {
+      res.send(result)
+    })
+
+  res.send('User updates')
+})
+
+app.get('/delete-user', async (req, res) => {
+  // await userModel.deleteOne({ username: 'pasta' }).then((result) => {
+  //   res.send(result)
+  // })
+  // -- findOneAndDelete
+  await userModel.findOneAndDelete({
+    username: 'pasta',
+  })
+
+  res.send('user deleted')
+})
+
 app.listen(3000)
